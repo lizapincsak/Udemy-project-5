991bdac31b0c346b7d5a824d290750a969b825ca
lizapincsak <liza.pincsak@gmail.com>
2021-08-03 13:05:17 -0500

    usecontext

--- src/components/MainHeader/Navigation.js
+++ src/components/MainHeader/Navigation.js
@@ -3,7 +3,7 @@ import AuthContext from '../../store/auth-context';
 
 import classes from './Navigation.module.css';
 
-const Navigation = (props) => {
+const Navigation = () => {
   const cxt = useContext(AuthContext);
   return (
           <nav className={classes.nav}>
@@ -20,7 +20,7 @@ const Navigation = (props) => {
             )}
             {cxt.isLoggedIn && (
               <li>
-                <button onClick={props.onLogout}>Logout</button>
+                <button onClick={cxt.onLogout}>Logout</button>
               </li>
             )}
           </ul>
